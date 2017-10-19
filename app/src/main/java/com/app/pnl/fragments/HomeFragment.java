cdedb0b47adcf90fc3153c9cff4a02d9c647eb89
shahsyed <shahsyed@axact.com>
2017-10-19 07:38:00 -0700

    Signed-off-by: shahsyed <shahsyed@axact.com>

--- app/src/main/java/com/app/pnl/fragments/HomeFragment.java
+++ app/src/main/java/com/app/pnl/fragments/HomeFragment.java
@@ -7,9 +7,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.app.pnl.R;
 import com.app.pnl.fragments.abstracts.BaseFragment;
+import com.app.pnl.ui.views.AnyTextView;
 import com.app.pnl.ui.views.TitleBar;
 
 import butterknife.BindView;
@@ -24,8 +26,14 @@ public class HomeFragment extends BaseFragment {
     LinearLayout btnPopular;
     @BindView(R.id.btn_services)
     LinearLayout btnServices;
+    @BindView(R.id.top_shelf)
+    LinearLayout topShelf;
     @BindView(R.id.btn_companies)
     LinearLayout btnCompanies;
+    @BindView(R.id.txt_favourite_count)
+    AnyTextView txtFavouriteCount;
+    @BindView(R.id.img_favourite_badge)
+    RelativeLayout imgFavouriteBadge;
     @BindView(R.id.btn_favourites)
     LinearLayout btnFavourites;
 
@@ -61,12 +69,16 @@ public class HomeFragment extends BaseFragment {
     }
 
 
-    @OnClick({R.id.btn_popular, R.id.btn_services, R.id.btn_companies, R.id.btn_favourites})
+
+
+    @OnClick({R.id.btn_popular, R.id.btn_services, R.id.top_shelf, R.id.btn_companies, R.id.btn_favourites})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.btn_popular:
                 break;
             case R.id.btn_services:
+                break;
+            case R.id.top_shelf:
                 break;
             case R.id.btn_companies:
                 break;
---
+++ app/src/main/java/com/app/pnl/fragments/ServicesFragment.java
@@ -0,0 +1,92 @@
+package com.app.pnl.fragments;
+
+import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.app.pnl.R;
+import com.app.pnl.entities.ServiceEnt;
+import com.app.pnl.fragments.abstracts.BaseFragment;
+import com.app.pnl.interfaces.RecyclerViewItemListener;
+import com.app.pnl.ui.viewbinders.viewbinders.ServicesBinder;
+import com.app.pnl.ui.views.CustomRecyclerView;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * Created on 10/19/2017.
+ */
+public class ServicesFragment extends BaseFragment implements RecyclerViewItemListener {
+    @BindView(R.id.rcy_services)
+    CustomRecyclerView rcyServices;
+    @BindView(R.id.btn_all_services)
+    Button btnAllServices;
+    private ArrayList<ServiceEnt> userCollections;
+
+    public static ServicesFragment newInstance() {
+        Bundle args = new Bundle();
+
+        ServicesFragment fragment = new ServicesFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_services, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        bindData();
+
+    }
+
+    private void bindData() {
+
+        userCollections.add(new ServiceEnt(R.drawable.hospital, getString(R.string.hospital)));
+        userCollections.add(new ServiceEnt(R.drawable.restaurant, getString(R.string.restaurant)));
+        userCollections.add(new ServiceEnt(R.drawable.bakery, getString(R.string.bakery)));
+        userCollections.add(new ServiceEnt(R.drawable.spa, getString(R.string.spa)));
+        userCollections.add(new ServiceEnt(R.drawable.cafe, getString(R.string.cafe)));
+        userCollections.add(new ServiceEnt(R.drawable.casino, getString(R.string.casino)));
+        userCollections.add(new ServiceEnt(R.drawable.construaction, getString(R.string.construction)));
+        userCollections.add(new ServiceEnt(R.drawable.store, getString(R.string.store)));
+        userCollections.add(new ServiceEnt(R.drawable.law, getString(R.string.law)));
+        userCollections.add(new ServiceEnt(R.drawable.traveling, getString(R.string.traveling)));
+        userCollections.add(new ServiceEnt(R.drawable.finance, getString(R.string.finance)));
+        userCollections.add(new ServiceEnt(R.drawable.pet, getString(R.string.pets)));
+        rcyServices.BindRecyclerView(new ServicesBinder(this), userCollections,
+                new GridLayoutManager(getDockActivity(), 3), new DefaultItemAnimator());
+
+    }
+
+
+    @OnClick(R.id.btn_all_services)
+    public void onViewClicked() {
+    }
+
+    @Override
+    public void onRecyclerItemClicked(Object Ent, int position) {
+
+    }
+}
